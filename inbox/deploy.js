3578cdaa6387e0554f7df38f2e285b25742ad888
Sam Smolkin <sam@future-precedent.org>
2018-08-29 23:20:00 -0400

    added deploy script

---
+++ inbox/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider( 
+  'normal cube seminar weird infant sweet firm velvet silly wing pyramid during',
+  'https://rinkeby.infura.io/v3/373ac1fa8ecf4e8f826021e681b18976'
+);
+
+const web3 = new Web3(provider);
+
+(async () => {
+  const accounts = await web3.eth.getAccounts();
+  
+  console.log('Attempting to deploy from account ', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!']})
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to ', result.options.address)
+})();
